e0365a0fe3c47010680c65f86086b1ade1a9134a
Rui Malemba <95936638+RuiYuriAfricano@users.noreply.github.com>
2025-05-08 10:16:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ class_names = ['cataract', 'diabetic_retinopathy', 'glaucoma', 'normal']
 
 # Função de predição
 def predict(model, img):
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Batch boyutu ekle
+    img2 = tf.keras.utils.load_img(img, target_size=(256, 256))
+    img_array = tf.keras.utils.img_to_array(img2)
     img_array = np.expand_dims(img_array, axis=0).astype(np.float32)
     predictions = model.predict(img_array)
     predicted_class = class_names[np.argmax(predictions[0])]
